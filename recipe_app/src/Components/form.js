415b9f7b968f4fd5e3301e28abfde44284d06d16
Courtney Dart <dart.courtney1@gmail.com>
2021-02-28 10:48:57 -0800

    Saved changes.

---
+++ recipe_app/src/Components/form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Form = () => {
+
+    return (
+        <div className="form-container">
+            <label>Recipe Name</label>
+            <input type='text'></input>
+            <label>Ingredients List</label>
+            <input type='text'></input>
+            <label>Instructions:</label>
+            <input type='text'></input>
+        </div>
+    );
+} 
+
+export default Form
