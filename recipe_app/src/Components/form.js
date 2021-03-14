8040499440af306a8f8fe9767390a4c9f79f3c05
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2021-03-14 16:27:49 -0700

    add state for form and ingredient input

--- recipe_app/src/Components/form.js
+++ recipe_app/src/Components/form.js
@@ -2,7 +2,14 @@ import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const RecipeForm = () => {
+const RecipeForm = ({inputText,setInputText, 
+                     ingredients, setIngredients,
+                     forms, setForms}) => {
+    
+const inputTextHandler = (e) => {
+    setInputText(e.target.value)
+}
+
 
     return (
         <Form>
@@ -11,16 +18,16 @@ const RecipeForm = () => {
                 <Form.Control type="text" placeholder="Banana bread"/>
             </Form.Group>
             <Form.Group>
-                <Form.Label>Ingredients List</Form.Label><br></br>
-                <Form.Control type="text" placeholder="Recipe Name" />
+                <Form.Control type="text" placeholder="Ingredient" value={inputText} onChange={inputTextHandler}/>
+                <Button primary lg>+</Button>
+                {/* <button onClick>Add Ingredient</button> */}
+                {/* <Form.Label>Ingredients List</Form.Label><br></br>
+                <Form.Control type="text" placeholder="Recipe Name" /> */}
             </Form.Group>
             <Form.Group>
                 <Form.Label>Instructions:</Form.Label><br></br>
                 <Form.Control as="textarea" rows={25} />    
             </Form.Group>
-            
-            
-            
         </Form>
     );
 } 
